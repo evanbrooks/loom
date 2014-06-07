1454afa7eda7932cf8552938143d5435e18d0299
Evan Brooks <brooks.ev@gmail.com>
2014-06-07 16:30:48 -0700

    disable ngrok temporarily

--- script/quick-server.js
+++ script/quick-server.js
@@ -26,7 +26,7 @@
 
     window.io = io;
 
-    window.staticRoot = '/Users/evan/Developer/loom/experiments/sample';
+    window.staticRoot = '/Users/evan/Developer/degreeProject';
     
     var socketScript  = fs.readFileSync('/Users/evan/Developer/loom/script/socketio-client.min.js');
     var ansibleScript = fs.readFileSync('/Users/evan/Developer/loom/script/ansible-client.js');
@@ -46,12 +46,13 @@
     var server = socketServer.listen(process.env.PORT || 3000, function() {
       var port = server.address().port;
       console.info('Local servet running at ' + port );
-      self.publish();
+      // self.publish();
     });
 
     io.sockets.on('connection', function (socket) {
 
-      console.log("SOCKET CONNECTION EVENT!")
+      // Getting "cannot call method log of null" error here for some reason
+      if (console) console.log("SOCKET CONNECTION EVENT!")
 
 
       socket.on('message', function(msg) {
